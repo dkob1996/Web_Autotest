407f98abb85eae663333d4c5d89dc66b5ab2f286
Dmitrii Kobozev <dmitrii_kobozev@Noutbuk-Dmitrii.local>
2024-01-14 21:50:00 +0600

    done first  attempt to multi browsing testing

---
+++ Sem2/task/test_1.py
@@ -0,0 +1,24 @@
+import yaml
+from module import Site
+
+
+with open ("/Users/dmitrii_kobozev/Desktop/Web_Autotests/Sem2/task/testdata.yaml") as f:
+    testdata = yaml.safe_load(f)
+site = Site(testdata["address"])
+
+
+def test_step1():
+    x_selector_1 = """//*[@id="login"]/div[1]/label/input"""
+    input1 = site.find_element('xpath', x_selector_1)
+    input1.send_keys('test')
+    x_selector_2 = """//*[@id="login"]/div[2]/label/input"""
+    input2 = site.find_element('xpath', x_selector_2)
+    input2.send_keys('test')
+    btn_selector = "button"
+    btn = site.find_element('css', btn_selector)
+    btn.click()
+    x_selector_3 = """//*[@id="app"]/main/div/div/div[2]/h2"""
+    err_label = site.find_element('xpath', x_selector_3)
+    assert err_label.text == '401'
+test_step1()
+#button 
